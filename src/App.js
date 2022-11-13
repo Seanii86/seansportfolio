fe4a06b399d658b53745f89e2a6ac4ddd8fbcbbb
Sean <jconnect7@gmail.com>
2022-11-12 19:29:52 -0800

    Attempting to fix SPA relation with gitpages 404 refresh error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Layout from './components/Layout'
 import './App.scss';
-import { Routes, Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import React from 'react'
 import Home from './components/home';
 import About from './components/About';
@@ -10,16 +10,17 @@ import Portfolio from './components/Portfolio';
 function App() {
   return (
     <>
-      <Routes>
+      <Switch>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/Portfolio' element={<Portfolio />} />
         </Route>
-      </Routes>
+      </Switch>
     </>
   );
 }
 
 export default App;
+
